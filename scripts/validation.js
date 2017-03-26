f776668f85efcfa8ff8090e3f4aedaa97e6fff96
long261vn <long261vn@yahoo.com>
2017-03-26 14:37:25 -0700

    Chap 12 without Challenge

---
+++ scripts/validation.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+    var App = window.App || {};
+    var Validation = {
+        isCompanyEmail: function (email) {
+            return /.+@bignerdranch\.com$/.test(email);
+        }
+    };
+
+    App.Validation = Validation;
+    window.App = App;
+})(window);
